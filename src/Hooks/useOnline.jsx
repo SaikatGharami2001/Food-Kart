0e1f83bd9acd90ceada6e8954dec57284d373d28
Saikat Gharami <saikatgharami2@gmail.com>
2025-09-01 02:07:32 +0530

    Files Added

---
+++ src/Hooks/useOnline.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const useOnline = () => {
+  return <div>useOnline</div>;
+};
+
+export default useOnline;
---
+++ src/Utils/contants.jsx
@@ -0,0 +1,5 @@
+export const LOGO = "";
+
+export const RES_API = "";
+
+export const RES_DATA = "";
