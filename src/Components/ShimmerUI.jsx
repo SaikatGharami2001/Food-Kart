1de7149b6fdfa6f87119eee01f4210ff49b3063e
Saikat Gharami <saikatgharami2@gmail.com>
2025-09-03 23:10:02 +0530

    Your message here

--- src/Components/ShimmerUI.jsx
+++ src/Components/ShimmerUI.jsx
@@ -4,12 +4,8 @@ const ShimmerCards = lazy(() => import("../Components/ShimmerCards"));
 
 const ShimmerUI = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-6">
-      <Suspense
-        fallback={
-          <h1 className="col-span-full text-center text-xl">Loading...</h1>
-        }
-      >
+    <Suspense fallback={<ShimmerCards />}>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-6">
         <ShimmerCards />
         <ShimmerCards />
         <ShimmerCards />
@@ -18,8 +14,8 @@ const ShimmerUI = () => {
         <ShimmerCards />
         <ShimmerCards />
         <ShimmerCards />
-      </Suspense>
-    </div>
+      </div>
+    </Suspense>
   );
 };
 
