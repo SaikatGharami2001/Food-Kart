8ada8ae63261e596b46a9942cf0ad1eeaddca896
Saikat Gharami <saikatgharami2@gmail.com>
2025-09-03 23:14:31 +0530

    Your message here

--- src/Components/Restaurant_Card.jsx
+++ src/Components/Restaurant_Card.jsx
@@ -1,26 +1,27 @@
 import { IMG_API } from "../Utils/constants";
 
 const Restaurant_Card = ({ restaurantInfo }) => {
-  const { name, cuisines, avgRating, id, sla } = restaurantInfo;
+  const { name, cuisines, avgRating, id, sla, cloudinaryImageId } =
+    restaurantInfo;
 
   return (
     <div className="max-w-xs rounded-2xl overflow-hidden shadow-lg bg-orange-100 hover:shadow-xl transition-shadow duration-300">
       {/* Image */}
       <img
-        src={IMG_API}
+        src={IMG_API + cloudinaryImageId}
         alt="Restaurant"
         className="w-full h-40 object-cover"
       />
 
       {/* Content */}
       <div className="p-4 space-y-2">
-        <h1 className="text-lg font-bold text-gray-800">{}</h1>
+        <h1 className="text-lg font-bold text-gray-800">{name}</h1>
 
         {/* Rating & Delivery */}
         <div className="flex justify-between items-center text-sm text-gray-600">
           <span className="flex items-center gap-1">⭐ {avgRating}</span>
           <span className="px-2 py-1 bg-green-100 text-green-700 rounded-full text-xs font-medium">
-            30 min
+            {sla.deliveryTime} Min
           </span>
         </div>
 
